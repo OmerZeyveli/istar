a4bdff225fdd02f2adb1af4cf800883a0efbf163
Riive <omer.zeyveli@gmail.com>
2025-05-18 09:33:45 +0300

    Update cleaning code
    Updated some comments and small fixes.

--- clean_training.sh
+++ clean_training.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-# Usage: bash clean_training_only.sh data/cell1/drop30_v1/
+# Usage: bash clean_training_only.sh data/demo/
 
 prefix=$1
 
 if [[ -z "$prefix" ]]; then
-  echo "❌ Error: No prefix provided. Usage: bash clean_training_only.sh data/cell1/drop30_v1/"
+  echo "❌ Error: No prefix provided. Usage: bash clean_training_only.sh data/demo/"
   exit 1
 fi
 
 echo "Cleaning training-related files in: $prefix"
 
-rm -rf "${prefix}states/"
-rm -f  "${prefix}embeddings-hist-raw.pickle"
+# Comment out the embeddings-hist-raw if you want to keep the histology features. This will help retraining the model with the same histology features but different gene features.
+rm -f "${prefix}embeddings-hist-raw.pickle"
+rm -f "${prefix}states/"
 
-echo "✅ Training-related files deleted (histology features kept)."
+echo "✅ Training-related files deleted"
